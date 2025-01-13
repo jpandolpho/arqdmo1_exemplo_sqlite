c0b7bdc18ffa8fb108131565b190e7197a608573
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-01-13 11:14:23 -0300

    Refatorando código para refletir mudança no banco.

--- app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/data/database/DatabaseHelper.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/data/database/DatabaseHelper.kt
@@ -8,22 +8,45 @@ class DatabaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_KEYS
 
     object DATABASE_KEYS {
         const val DATABASE_NAME = "exemplo_database.db"
-        const val DATABASE_VERSION = 1
+        const val DATABASE_VERSION = 2
         const val TABLE_NAME = "tb_meu_dado"
+        const val COLUMN_ID = "id"
         const val COLUMN_TEXTO = "texto"
     }
 
     private companion object {
         const val CREATE_TABLE_V1 = "CREATE TABLE ${DATABASE_KEYS.TABLE_NAME} (${DATABASE_KEYS.COLUMN_TEXTO} TEXT)"
+        const val CREATE_TABLE_V2 = "CREATE TABLE ${DATABASE_KEYS.TABLE_NAME} (" +
+                "${DATABASE_KEYS.COLUMN_ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "${DATABASE_KEYS.COLUMN_TEXTO} TEXT NOT NULL)"
+
         const val DROP_TABLE = "DROP TABLE IF EXISTS ${DATABASE_KEYS.TABLE_NAME}";
     }
 
     override fun onCreate(db: SQLiteDatabase) {
-        db.execSQL(CREATE_TABLE_V1)
+        db.execSQL(CREATE_TABLE_V2)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
-        db.execSQL(DROP_TABLE);
-        onCreate(db);
+        when {
+            oldVersion < 2 -> {
+                updateToVersion2(db)
+            }
+        }
+    }
+
+    private fun updateToVersion2(db: SQLiteDatabase) {
+        var sql = "ALTER TABLE ${DATABASE_KEYS.TABLE_NAME} RENAME TO ${DATABASE_KEYS.TABLE_NAME}_OLD"
+        db.execSQL(sql)
+
+        db.execSQL(CREATE_TABLE_V2)
+
+        sql = "INSERT INTO ${DATABASE_KEYS.TABLE_NAME} (${DATABASE_KEYS.COLUMN_TEXTO}) " +
+                "SELECT ${DATABASE_KEYS.COLUMN_TEXTO} " +
+                "FROM ${DATABASE_KEYS.TABLE_NAME}_OLD"
+        db.execSQL(sql)
+
+        sql = "${DROP_TABLE}_OLD"
+        db.execSQL(sql)
     }
 }
