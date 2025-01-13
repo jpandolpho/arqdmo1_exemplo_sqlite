05c525179e409994a5b9a040baaf3032905afd31
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-01-13 10:43:54 -0300

    Refatorando código para utilizar a DetailsActivity e para carregar dados no início do aplicativo.

--- app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/main/MainActivity.kt
@@ -1,17 +1,23 @@
 package br.edu.ifsp.dmo1.exemplosqlite.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import br.edu.ifsp.dmo1.exemplosqlite.databinding.ActivityMainBinding
+import br.edu.ifsp.dmo1.exemplosqlite.ui.details.DetailsActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
     private lateinit var adapter: MeuDadoAdapter
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +29,26 @@ class MainActivity : AppCompatActivity() {
         setupRecyclerView()
         setupObservers()
         setupListeners()
+        setupLauncher()
+        viewModel.load()
+    }
+
+    private fun setupLauncher() {
+        resultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback {
+                if (it.resultCode == RESULT_OK) {
+                    val texto = it.data?.getStringExtra("texto") ?: ""
+                    viewModel.addDado(texto)
+                }
+            }
+        )
     }
 
     private fun setupListeners() {
         binding.buttonAdd.setOnClickListener {
-            viewModel.addDado("Novo texto")
+            val mIntent = Intent(this, DetailsActivity::class.java)
+            resultLauncher.launch(mIntent)
         }
     }
 
