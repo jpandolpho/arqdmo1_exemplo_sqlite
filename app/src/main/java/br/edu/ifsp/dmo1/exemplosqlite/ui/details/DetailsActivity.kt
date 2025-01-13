c1d30d6486a6347c87ec83f4b93d46884f2ebf22
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-01-13 10:39:49 -0300

    Definindo lógica da DetailsActivity.

--- app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/details/DetailsActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/details/DetailsActivity.kt
@@ -1,21 +1,37 @@
 package br.edu.ifsp.dmo1.exemplosqlite.ui.details
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import br.edu.ifsp.dmo1.exemplosqlite.R
+import br.edu.ifsp.dmo1.exemplosqlite.databinding.ActivityDetailsBinding
 
-class DetailsActivity : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var binding: ActivityDetailsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == binding.buttonSave.id) {
+            val string = binding.textTexto.text.toString()
+            if (string.isNotEmpty()) {
+                val mIntent = Intent()
+                mIntent.putExtra("texto", string)
+                setResult(RESULT_OK, mIntent)
+            } else {
+                Toast.makeText(this, "Não foi inserido texto.", Toast.LENGTH_SHORT).show()
+                setResult(RESULT_CANCELED)
+            }
+            finish()
         }
     }
 }
