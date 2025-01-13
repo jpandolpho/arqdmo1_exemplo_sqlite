a29bf8c7be19c294035e1ff93083b8c3ad4586fa
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-01-13 10:26:33 -0300

    Definindo lógica para a MainActivity e MainViewModel.

--- app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/main/MainActivity.kt
@@ -1,21 +1,47 @@
 package br.edu.ifsp.dmo1.exemplosqlite.ui.main
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import br.edu.ifsp.dmo1.exemplosqlite.R
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import br.edu.ifsp.dmo1.exemplosqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: MeuDadoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        setupRecyclerView()
+        setupObservers()
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.buttonAdd.setOnClickListener {
+            viewModel.addDado("Novo texto")
         }
     }
+
+    private fun setupObservers() {
+        viewModel.dados.observe(this, Observer {
+            adapter.updateDados(it)
+        })
+    }
+
+    private fun setupRecyclerView() {
+        adapter = MeuDadoAdapter(mutableListOf())
+        binding.listDados.adapter = adapter
+        binding.listDados.layoutManager = LinearLayoutManager(this)
+    }
+
+
 }
--- app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/main/MainViewModel.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/main/MainViewModel.kt
@@ -1,4 +1,25 @@
 package br.edu.ifsp.dmo1.exemplosqlite.ui.main
 
-class MainViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import br.edu.ifsp.dmo1.exemplosqlite.data.model.MeuDado
+import br.edu.ifsp.dmo1.exemplosqlite.data.repository.MeuDadoRepository
+
+class MainViewModel(application: Application) : AndroidViewModel(application){
+    private lateinit var repository: MeuDadoRepository
+
+    private val _dados = MutableLiveData<List<MeuDado>>()
+    val dados: LiveData<List<MeuDado>>
+        get() = _dados
+
+    init {
+        repository = MeuDadoRepository(application)
+    }
+
+    fun addDado(texto: String) {
+        repository.addMeuDado(MeuDado(texto))
+        _dados.value = repository.getAllMeusDados()
+    }
 }
