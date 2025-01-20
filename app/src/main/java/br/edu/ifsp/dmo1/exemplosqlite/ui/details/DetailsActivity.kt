09772dcbb343e53b13fa514df938332cba5723af
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-01-20 09:20:13 -0300

    Atualizando a DetailsActivity para permitir alterar dados.

--- app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/details/DetailsActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/details/DetailsActivity.kt
@@ -3,9 +3,17 @@ package br.edu.ifsp.dmo1.exemplosqlite.ui.details
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
 import android.view.View.OnClickListener
+import android.view.View.VISIBLE
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import br.edu.ifsp.dmo1.exemplosqlite.R
 import br.edu.ifsp.dmo1.exemplosqlite.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity(), OnClickListener {
@@ -17,8 +25,12 @@ class DetailsActivity : AppCompatActivity(), OnClickListener {
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        verifyBundle()
+
         binding.buttonSave.setOnClickListener(this)
     }
+
+
 
     override fun onClick(v: View) {
         if (v.id == binding.buttonSave.id) {
@@ -26,12 +38,30 @@ class DetailsActivity : AppCompatActivity(), OnClickListener {
             if (string.isNotEmpty()) {
                 val mIntent = Intent()
                 mIntent.putExtra("texto", string)
+
+                if (binding.buttonSave.text.toString().equals("Atualizar")){
+                    val id = binding.textId.text.toString().toInt()
+                    mIntent.putExtra("id", id)
+                }
+
                 setResult(RESULT_OK, mIntent)
             } else {
-                Toast.makeText(this, "Não foi inserido texto.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Não foi possível salvar os dados.", Toast.LENGTH_SHORT).show()
                 setResult(RESULT_CANCELED)
             }
             finish()
+        }
+    }
+
+    private fun verifyBundle() {
+        if (intent.extras != null) {
+            val id = intent.getIntExtra("id", -1)
+            val texto = intent.getStringExtra("texto")
+
+            binding.buttonSave.setText("Atualizar")
+            binding.textlayoutId.visibility = VISIBLE
+            binding.textId.setText(id.toString())
+            binding.textTexto.setText(texto)
         }
     }
 }
