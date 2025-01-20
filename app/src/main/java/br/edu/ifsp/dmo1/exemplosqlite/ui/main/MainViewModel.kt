a7357fed635736372dec54c6b8c617ffa7a93bf8
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-01-20 09:30:22 -0300

    Adicionando funcionalidade de exclusão a MainViewModel.

--- app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/main/MainViewModel.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/ui/main/MainViewModel.kt
@@ -9,10 +9,17 @@ import br.edu.ifsp.dmo1.exemplosqlite.data.repository.MeuDadoRepository
 
 class MainViewModel(application: Application) : AndroidViewModel(application){
     private var repository: MeuDadoRepository
+    private var toDeleteMeuDado: MeuDado? = null
 
     private val _dados = MutableLiveData<List<MeuDado>>()
     val dados: LiveData<List<MeuDado>>
         get() = _dados
+
+    private val _toDeleteTexto = MutableLiveData<String>()
+    val toDeleteTexto: LiveData<String> = _toDeleteTexto
+
+    private val _deleted = MutableLiveData<Boolean>()
+    val deleted: LiveData<Boolean> = _deleted
 
     init {
         repository = MeuDadoRepository(application)
@@ -30,5 +37,21 @@ class MainViewModel(application: Application) : AndroidViewModel(application){
     fun updateDado(id: Int, texto: String) {
         repository.updateMeuDado(MeuDado(id,texto))
         load()
+    }
+
+    fun notifyDelete(id: Int) {
+        toDeleteMeuDado = repository.getMeuDadoById(id)
+        if (toDeleteMeuDado != null) {
+            _toDeleteTexto.value = toDeleteMeuDado!!.texto
+        }
+    }
+
+    fun confirmDelete() {
+        if (toDeleteMeuDado != null) {
+            repository.deleteMeuDado(toDeleteMeuDado!!)
+            toDeleteMeuDado = null
+            _deleted.value = true
+            load()
+        }
     }
 }
