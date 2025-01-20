8eab0f738136b7a054ef5737aa242b8f742e6913
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2025-01-20 10:16:25 -0300

    Atualizando dao para refletir mudança no banco.

--- app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/data/database/MeuDadoDao.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/exemplosqlite/data/database/MeuDadoDao.kt
@@ -9,6 +9,7 @@ class MeuDadoDao(private val dbHelper: DatabaseHelper) {
         val db = dbHelper.writableDatabase
         val values = ContentValues().apply {
             put(DatabaseHelper.DATABASE_KEYS.COLUMN_TEXTO, meuDado.texto)
+            put(DatabaseHelper.DATABASE_KEYS.COLUMN_NUMERO, meuDado.numero)
         }
         db.insert(DatabaseHelper.DATABASE_KEYS.TABLE_NAME, null, values)
     }
@@ -17,7 +18,8 @@ class MeuDadoDao(private val dbHelper: DatabaseHelper) {
         val db = dbHelper.readableDatabase
         val columns = arrayOf(
             DatabaseHelper.DATABASE_KEYS.COLUMN_ID,
-            DatabaseHelper.DATABASE_KEYS.COLUMN_TEXTO
+            DatabaseHelper.DATABASE_KEYS.COLUMN_TEXTO,
+            DatabaseHelper.DATABASE_KEYS.COLUMN_NUMERO
         )
 
         val cursor = db.query(DatabaseHelper.DATABASE_KEYS.TABLE_NAME,
@@ -32,7 +34,10 @@ class MeuDadoDao(private val dbHelper: DatabaseHelper) {
         cursor.use {
             while (it.moveToNext()) {
                 dados.add(
-                    MeuDado(id = it.getInt(0), texto = it.getString(1))
+                    MeuDado(
+                        id = it.getInt(0),
+                        texto = it.getString(1),
+                        numero = it.getInt(2))
                 )
             }
         }
@@ -45,7 +50,8 @@ class MeuDadoDao(private val dbHelper: DatabaseHelper) {
         val db = dbHelper.readableDatabase
         val columns = arrayOf(
             DatabaseHelper.DATABASE_KEYS.COLUMN_ID,
-            DatabaseHelper.DATABASE_KEYS.COLUMN_TEXTO
+            DatabaseHelper.DATABASE_KEYS.COLUMN_TEXTO,
+            DatabaseHelper.DATABASE_KEYS.COLUMN_NUMERO
         )
 
         val where = "${DatabaseHelper.DATABASE_KEYS.COLUMN_ID} = ?"
@@ -63,7 +69,10 @@ class MeuDadoDao(private val dbHelper: DatabaseHelper) {
 
         cursor.use {
             dado = if (cursor.moveToNext()) {
-                MeuDado(cursor.getInt(0), cursor.getString(1))
+                MeuDado(
+                    cursor.getInt(0),
+                    cursor.getString(1),
+                    cursor.getInt(2))
             }else {
                 null
             }
@@ -76,6 +85,7 @@ class MeuDadoDao(private val dbHelper: DatabaseHelper) {
         val db = dbHelper.writableDatabase
         val values = ContentValues().apply {
             put(DatabaseHelper.DATABASE_KEYS.COLUMN_TEXTO, meuDado.texto)
+            put(DatabaseHelper.DATABASE_KEYS.COLUMN_NUMERO, meuDado.numero)
         }
 
         val where = "${DatabaseHelper.DATABASE_KEYS.COLUMN_ID} = ?"
